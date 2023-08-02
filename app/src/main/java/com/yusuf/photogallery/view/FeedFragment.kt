1b45fe165146c35ba6f8563b1387f88c8c7896be
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-02 10:50:10 +0300

    Navigation Added

---
+++ app/src/main/java/com/yusuf/photogallery/view/FeedFragment.kt
@@ -0,0 +1,17 @@
+package com.yusuf.photogallery.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yusuf.photogallery.R
+
+class FeedFragment : Fragment(R.layout.fragment_feed) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
