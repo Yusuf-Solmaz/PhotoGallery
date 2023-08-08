7109cbe40687090ababf09e3b1e2a09dbb375dab
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-08 10:42:46 +0300

    Inject Fragment Factory

--- app/src/main/java/com/yusuf/photogallery/view/MainActivity.kt
+++ app/src/main/java/com/yusuf/photogallery/view/MainActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yusuf.photogallery.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @Inject
+    lateinit var fragmentFactory: ImageFragmentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportFragmentManager.fragmentFactory = fragmentFactory
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
