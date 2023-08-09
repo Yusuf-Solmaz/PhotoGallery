c83f41e84bb87545f8c03116ccb189e2c43e5f02
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-09 10:33:04 +0300

    Sharing Image Fragment Completed

--- app/src/main/java/com/yusuf/photogallery/view/SharingImage.kt
+++ app/src/main/java/com/yusuf/photogallery/view/SharingImage.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
 import com.yusuf.photogallery.R
 import com.yusuf.photogallery.databinding.FragmentSharingImageBinding
+import com.yusuf.photogallery.util.Status
+import com.yusuf.photogallery.viewmodel.ImageViewModel
 import javax.inject.Inject
 
 
@@ -17,13 +22,20 @@ class SharingImage @Inject constructor(
     private val glide: RequestManager
 ) : Fragment(R.layout.fragment_sharing_image) {
 
-    private lateinit var fragmentBinding: FragmentSharingImageBinding
+    private  var fragmentBinding : FragmentSharingImageBinding? = null
+
+    private lateinit var viewModel: ImageViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentSharingImageBinding.bind(view)
         fragmentBinding = binding
+
+        subscribeToObservers()
+
+        viewModel = ViewModelProvider(requireActivity()).get(ImageViewModel::class.java)
+
 
         binding.imageView.setOnClickListener {
             findNavController().navigate(SharingImageDirections.actionSharingImageToImageSearch())
@@ -36,6 +48,43 @@ class SharingImage @Inject constructor(
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+
+        binding.saveButton.setOnClickListener {
+            viewModel.makeImage(binding.titleText.text.toString(),binding.placeText.text.toString(),binding.dateText.text.toString())
+
+
+        }
+    }
+
+    private fun subscribeToObservers(){
+        viewModel.selectedImageUrl.observe(viewLifecycleOwner, Observer {url ->
+            fragmentBinding?.let {binding ->
+                glide.load(url).into(binding.imageView)
+            }
+        })
+
+        viewModel.insertImageMessage.observe(viewLifecycleOwner, Observer {
+            when(it.status){
+                Status.SUCCESS -> {
+                    Toast.makeText(requireContext(),"Success!",Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                    viewModel.resetInsertArtMSG()
+                }
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(),it.message ?: "Failed!",Toast.LENGTH_LONG).show()
+                    //viewModel.resetInsertArtMSG()
+                }
+
+                Status.LOADING ->{
+
+                }
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
     }
 
 }
