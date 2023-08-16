f503749a51c390499b5990515d17711d15bda018
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-08-16 11:30:53 +0300

    Debug

--- app/src/androidTest/java/com/yusuf/photogallery/HiltExtension.kt
+++ app/src/androidTest/java/com/yusuf/photogallery/HiltExtension.kt
@@ -6,8 +6,11 @@ import androidx.annotation.StyleRes
 import androidx.core.util.Preconditions
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.testing.EmptyFragmentActivity
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 /**
  * launchFragmentInContainer from the androidx.fragment:fragment-testing library
@@ -18,21 +21,19 @@ import androidx.test.core.app.ApplicationProvider
  * [HiltTestActivity] in the debug folder and include it in the debug AndroidManifest.xml file
  * as can be found in this project.
  */
+
 inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     fragmentArgs: Bundle? = null,
     @StyleRes themeResId: Int = androidx.fragment.testing.manifest.R.style.FragmentScenarioEmptyFragmentActivityTheme,
     factory: FragmentFactory,
-    crossinline action: Fragment.() -> Unit = {}
+    crossinline action: T.() -> Unit = {}
 ) {
     val startActivityIntent = Intent.makeMainActivity(
         ComponentName(
             ApplicationProvider.getApplicationContext(),
             HiltTestActivity::class.java
         )
-    ).putExtra(
-        "androidx.fragment.app.testing.FragmentScenario.EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY",
-        themeResId
-    )
+    ).putExtra(EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY, themeResId)
 
     ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
         activity.supportFragmentManager.fragmentFactory = factory
@@ -46,6 +47,6 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
             .add(android.R.id.content, fragment, "")
             .commitNow()
 
-        fragment.action()
+        (fragment as T).action()
     }
 }
--- app/src/androidTest/java/com/yusuf/photogallery/imageFragmentTest/ImageSharingFragmentTest.kt
+++ app/src/androidTest/java/com/yusuf/photogallery/imageFragmentTest/ImageSharingFragmentTest.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.FragmentFactory
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.yusuf.photogallery.launchFragmentInHiltContainer
 import com.yusuf.photogallery.view.ImageFragmentFactory
 import com.yusuf.photogallery.view.SharingImage
@@ -20,7 +25,9 @@ import org.junit.Test
 import org.mockito.Mockito
 import javax.inject.Inject
 import com.yusuf.photogallery.R
+import com.yusuf.photogallery.getOrAwaitValue
 import com.yusuf.photogallery.repo.FakeImageRepository
+import com.yusuf.photogallery.roomdb.Image
 import com.yusuf.photogallery.view.SharingImageDirections
 import com.yusuf.photogallery.viewmodel.ImageViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -74,26 +81,26 @@ class ImageSharingFragmentTest {
         Mockito.verify(navController).popBackStack()
     }
 
-   /*  @Test
+     @Test
     fun testSave() {
-        val testViewModel = ArtViewModel(FakeArtRepositoryAndroid())
-        launchFragmentInHiltContainer<ArtDetailsFragment>(
-            factory = fragmentFactory
-        ) {
+         val testViewModel = ImageViewModel(FakeImageRepository())
+         launchFragmentInHiltContainer<SharingImage>(
+             factory = fragmentFactory
+         ) {
             viewModel = testViewModel
         }
 
-        onView(withId(R.id.nameText)).perform(replaceText("Mona Lisa"))
-        onView(withId(R.id.artistText)).perform(replaceText("Da Vinci"))
-        onView(withId(R.id.yearText)).perform(replaceText("1700"))
+        onView(withId(R.id.imageName)).perform(replaceText("Mona Lisa"))
+        onView(withId(R.id.placeText)).perform(replaceText("Da Vinci"))
+        onView(withId(R.id.dateText)).perform(replaceText("1700"))
         onView(withId(R.id.saveButton)).perform(click())
 
-        assertThat(testViewModel.artList.getOrAwaitValue()).contains(
-            Art(
+        assertThat(testViewModel.imageList.getOrAwaitValue()).contains(
+            Image(
                 "Mona Lisa",
                 "Da Vinci",
-                1700,"")
+                "1700","")
         )
 
-    }*/
+    }
 }
